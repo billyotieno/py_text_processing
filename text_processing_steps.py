6662610bcd89fc6da5720179bf9c532f257abd43
Billy Otieno <billybrian94@gmail.com>
2022-11-14 14:20:07 +0300

    added: expanding contractions function

---
+++ text_processing_steps.py
@@ -0,0 +1,10 @@
+import re
+import string
+import numpy as np
+import pandas as pd
+from subprocess import check_output
+import nltk
+from collections import Counter
+from nltk import tokenize
+
+# Step 1. Converting All Letters to Lower Case
